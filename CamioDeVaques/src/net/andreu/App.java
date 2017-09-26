572c7d925e60af5a66e13a334bc7acc2e7a9f33d
JordiBeCo <32203120+JordiBeCo@users.noreply.github.com>
2017-09-26 19:34:08 +0200

    Update App.java

--- CamioDeVaques/src/net/andreu/App.java
+++ CamioDeVaques/src/net/andreu/App.java
@@ -1,4 +1,4 @@
-package net.andreu;
+mpackage net.andreu;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,4 +25,4 @@ public class App {
 	}
 
 }
-//afegeixo un comentari
+//elimino el comentari
